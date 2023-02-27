5b65bab13d801f60607221572a3a5424f4afc6f0
Carbon45 <thisisbusiness45@gmail.com>
2023-02-27 17:38:44 +0800

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(n) {
+    if (n < 0) {
+      return "OOPS";
+    } else if (n === 0) {
+      return 0;
+    } else if (n === 1 || n === 2) {
+      return 1;
+    } else if (typeof n === "string") {
+      n = parseInt(n);
+    }
+  
+    let fibArr = [0, 1, 1];
+  
+    for (let i = 3; i <= n; i++) {
+      fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+    }
+  
+    return fibArr[n];
+  };
+  
 // Do not edit below this line
 module.exports = fibonacci;
