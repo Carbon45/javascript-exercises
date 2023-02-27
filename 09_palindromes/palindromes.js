abcaf7e87ffa6fe200290c216b3567ea3aa863d2
Carbon45 <thisisbusiness45@gmail.com>
2023-02-27 16:58:51 +0800

    done with palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove all non-alphanumeric characters from string
+    const cleanStr = str.replace(/[^0-9a-z]/gi, '');
+  
+    // Check if cleaned string is a palindrome
+    const reversedStr = cleanStr.split('').reverse().join('');
 
-};
+    if (cleanStr.toLowerCase() === reversedStr.toLowerCase()) {
+        return true;
+    }
+    else {
+        return false;
+    }
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
