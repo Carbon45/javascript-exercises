3a2b5a87fd93a08ccfde86e26b55e4183168e765
Carbon45 <thisisbusiness45@gmail.com>
2023-02-27 16:38:26 +0800

    done with calculator stuff

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+  let totalSum = 0;
+
+	for (let i = 0; i < numArr.length; i++) {
+    totalSum += numArr[i];
+  }
+
+  return totalSum;
 };
 
-const multiply = function() {
+const multiply = function(numArr) {
+  let totalSum = 1;
 
+  for (let i = 0; i < numArr.length; i++) {
+    totalSum *= numArr[i];
+  }
+
+  return totalSum;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let result = 1;
+
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+
+    let result = 1;
+
+    for (let i = num; i >= 1; i--) {
+      result *= i;
+    }
+  
+    return result;
 };
 
 // Do not edit below this line
