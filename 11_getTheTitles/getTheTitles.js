74813f3469ca58e90e54f65cbfc11f224d72299e
Carbon45 <thisisbusiness45@gmail.com>
2023-02-27 18:14:16 +0800

    done with the titles stuff

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
 
+    // basically: return book.title
+
+    // 1. getting the books array
+    // 2. looping through each book object
+    // 3. getting the title from each book by looping through all of them
+    // 4. returns all of the titles
 };
 
 // Do not edit below this line
