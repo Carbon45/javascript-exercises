6e4731fc172c8e574113790676eeaebfb203315c
Carbon45 <thisisbusiness45@gmail.com>
2023-02-28 18:12:37 +0800

    done with findTheOldest project

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+function findTheOldest(people) {
+  const currentYear = new Date().getFullYear();
+  let oldestPerson = people[0];
 
-};
+  for (let i = 0; i < people.length; i++) {
+    let person = people[i];
 
-// Do not edit below this line
+    let age = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+
+    let oldestAge = (oldestPerson.yearOfDeath || currentYear) - oldestPerson.yearOfBirth;
+    
+    if (age > oldestAge) {
+      oldestPerson = person;
+    }
+  }
+
+  return oldestPerson;
+
+}
+// Do not edit below this line  
 module.exports = findTheOldest;
